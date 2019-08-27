7202d9abc9138a741fb0741bf7e76357fe8f7765
xiaodaobiepao <1310851368@qq.com>
2019-08-27 11:14:02 +0800

    add script

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ const Admin = require('../controller/admin/admin')
 const router = new Router()
 
 
-router.post('/login', Admin.login)
+router.get('/login', Admin.login)
 
 module.exports = router.routes()
 

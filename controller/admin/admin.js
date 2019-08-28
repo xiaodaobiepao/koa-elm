b5632d5a52fcddbcbb04017327915a951a2f98d2
xiaodaobiepao <1310851368@qq.com>
2019-08-28 20:50:31 +0800

    add post login

--- controller/admin/admin.js
+++ controller/admin/admin.js
@@ -7,8 +7,23 @@ class Admin extends AddressComponent {
         this.login = this.login.bind(this)
     }
     async login(ctx, next) {
-        ctx.type = 'html'
-        ctx.body = '<h1>登录</h1>'
+        // ctx.type = 'html'
+        // ctx.body = '<h1>登录</h1>'
+      const {user_name, password, status = 1} = ctx.request.fields
+      try {
+        if (!user_name) {
+          throw new Error('用户名不能为空')
+        } else if (!password) {
+          throw new Error('密码不能为空')
+        }
+      } catch (error) {
+        console.log(error)
+        ctx.body = {
+          status: 0,
+          type: 'GET_ERROR_PARAM',
+          message: error.message
+        }
+      }
     }
 }
 
